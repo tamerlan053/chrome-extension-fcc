3461dcd93612b5d5928732e98bcce9740f09f23c
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-14 22:00:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-let inputBtn = document.getElementById("input-Btn")
+let myLeads = []
 let inputEl = document.getElementById("input-el")
-let myLeads = ["www.google.com", "www.vk.com", "www.binance.com"]
-const ulEl = document.getElementById("ul-el")
+let ulEl = document.getElementById("ul-el")
 
-console.log(ulEl)
+let inputBtn = document.getElementById("input-btn");
 
-inputBtn = addEventListener("click", function() {
+
+
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
+function renderLeads() {
+
+let myList = ""    
+
 for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    myList += "<li><a target='_blank' href='"+ myLeads[i]+ "'>" + myLeads[i] + "</a></li>"
+  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" // one method, below is the second one
+  /*const li = document.createElement("li")
+  li.textContent = myLeads[i]
+  ulEl.append(li) */
+}
+
+ulEl.innerHTML = myList
 }
