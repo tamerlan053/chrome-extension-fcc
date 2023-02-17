d8087bf7206ec9dbdfd54f4bb889233765390d1e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-17 20:12:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ let myLeads = []
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 
-let inputBtn = document.getElementById("input-btn");
+let inputBtn = document.getElementById("input-btn")
 
-
+let item = localStorage.setItem("myLeads", "www.google.com")
+console.log(item)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
