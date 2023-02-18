aabbe34a30a05bdfafd11637ae5ac666d45d2e46
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-18 22:25:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,18 +17,7 @@ function renderLeads() {
 
 let myList = ""    
 
-for (let i = 0; i < myLeads.length; i++) {
-//myList += "<li><a target='_blank' href='"+ myLeads[i]+ "'>" + myLeads[i] + "</a></li>"
-myList += `<li>
-                <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
-           </li>`
 
-
-  //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" // one method, below is the second one
-  /*const li = document.createElement("li")
-  li.textContent = myLeads[i]
-  ulEl.append(li) */
-}
 
 ulEl.innerHTML = myList
 }
